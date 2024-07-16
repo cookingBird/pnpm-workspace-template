f86df759f1a728c4e6420fd0192a297804ce9b8a
Deng Tao <d17381844023@gmail.com>
2024-07-16 17:30:41 +0800

    chore(project): add babel and webpack common config

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,2 +1,39 @@
 /**@type { import("webpack").Configuration } */
-module.exports = {};
+module.exports = {
+  module: [
+    {
+      test: /\.jsx?$/,
+      use: ['babel-loader'],
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.tsx?$/,
+      use: [
+        'babel-loader',
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset',
+      parser: {
+        dataUrlCondition: {
+          maxSize: 50 * 1024, // <N>kb
+        },
+      },
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/source',
+    },
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
